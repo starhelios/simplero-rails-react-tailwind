360d36cf77819b1fd924079ff93bb8446c02cba7
Jackson Noda <starhelios94@gmail.com>
2022-09-01 17:03:18 -0400

    create navigation bar and link component

---
+++ app/components/links/button_link_component.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class Links::ButtonLinkComponent < ViewComponent::Base
+  def initialize(text:, link:)
+    @text = text
+    @link = link
+  end
+
+end
---
+++ test/components/links/button_link_component_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class Links::ButtonLinkComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(Links::ButtonLinkComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
